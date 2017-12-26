01ffc89fd00c340e300133f68c52a2d08b3cb82a
Jasio Wu <wbcyclist@gmail.com>
2017-12-26 10:14:52 +0800

    update 3.8.4

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.8.3",
-"md5":"dc1b5dc23ecc13dce8a7fb9237b1f1b7",
+"version":"3.8.4",
+"md5":"1f1843ca4640a64287b1096741b66899",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2017-12-14_21:00:32"
+"build_date":"2017-12-26_10:51:09"
 }
