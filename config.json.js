1a3b1ac4433c1349249bda88dcbc238930753494
Jasio Wu <wbcyclist@gmail.com>
2018-01-03 14:29:11 +0800

    update gfwlist.conf

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"3.8.4",
-"md5":"1f1843ca4640a64287b1096741b66899",
+"md5":"9b0ffd37c2d5138811304fabbe225985",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2017-12-26_10:51:09"
+"build_date":"2018-01-03_15:38:36"
 }
