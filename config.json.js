0bb0262d379e353192d15d12bf8b753017fa7d86
Jasio Wu <wbcyclist@gmail.com>
2018-01-26 17:03:50 +0800

    更新配置文件

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"3.8.4",
-"md5":"9b0ffd37c2d5138811304fabbe225985",
+"md5":"af818f80385b947f4e6e175271e1624a",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-01-03_15:38:36"
+"build_date":"2018-01-26_17:03:11"
 }
