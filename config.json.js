2cd43c84c41b87f4840bbda26d02590916c5059c
Jasio Wu <wbcyclist@gmail.com>
2018-02-10 16:13:41 +0800

    update rules

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"3.8.4",
-"md5":"af818f80385b947f4e6e175271e1624a",
+"md5":"be1eb4a4bc6f28cf003b0a22aa762089",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-01-26_17:03:11"
+"build_date":"2018-02-10_16:13:10"
 }
