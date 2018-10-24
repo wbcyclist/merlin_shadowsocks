05b76892fc30100e0cb0002f76ba482082af9279
Jasio Wu <wbcyclist@gmail.com>
2018-10-24 15:43:48 +0800

    update 4.1.4

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"4.1.3",
-"md5":"070e86216f5575d560ff5f63c1223a5e",
+"version":"4.1.4",
+"md5":"deffae990b2d7de65c8a79b96de0db13",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-10-10_17:24:37"
+"build_date":"2018-10-24_15:56:04"
 }
