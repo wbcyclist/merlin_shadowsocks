1d89b7c59a6e479f014abdfdd2b7a734d0d1a080
Jasio Wu <wbcyclist@gmail.com>
2019-10-29 18:36:11 +0800

    arm380 v4.2.0

---
+++ arm380/config.json.js
@@ -0,0 +1,9 @@
+{
+"version":"4.2.0",
+"md5":"6d53a5da3062201eaa0a7dfdea0aa7a0",
+"home_url":"Main_Ss_Content.asp",
+"title":"科学上网",
+"description":"科学上网",
+"tar_url": "https://raw.githubusercontent.com/wbcyclist/merlin_shadowsocks/master/arm380/shadowsocks.tar.gz",
+"build_date":"2019-10-29_18:13:02"
+}
--- config.json.js
+++
@@ -1,8 +0,0 @@
-{
-"version":"4.1.7",
-"md5":"7372fc20a3d6e0a868a9bbdb5494f9dd",
-"home_url":"Main_Ss_Content.asp",
-"title":"科学上网",
-"description":"科学上网",
-"build_date":"2018-11-28_14:35:48"
-}
