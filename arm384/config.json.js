49f6f56e6973ad34a95a638bc7ea8aa371451ce4
Jasio Wu <wbcyclist@gmail.com>
2020-01-16 15:40:03 +0800

    arm384 v1.0.4

--- arm384/config.json.js
+++ arm384/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"1.0.3",
-"md5":"963d7ee2b452bc1ef69dd6cce2635f70",
+"version":"1.0.4",
+"md5":"0ceb599e97d970e4899f1fe85c94d18a",
 "home_url":"Module_shadowsocks.asp",
 "title":"科学上网",
 "description":"科学上网",
 "tar_url": "https://raw.githubusercontent.com/wbcyclist/merlin_shadowsocks/master/arm384/shadowsocks.tar.gz",
-"build_date":"2019-10-30_10:35:49"
+"build_date":"2020-01-16_15:39:08"
 }
